c7859d3ed2a0c4afa2f735eea1c09e9ebb5656f8
Rogerio Lamarques <rogerio.lamarques.ext@infracommerce.com.br>
2022-06-13 15:33:53 -0300

    Documentado sentido de rotas

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => 'api'], function($router) {
+//Rotas de Autenticação
+Route::group(['middleware' => 'api'], function() {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout']);
